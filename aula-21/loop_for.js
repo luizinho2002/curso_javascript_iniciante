a65ad77009d8465fe9989ce524211bfedb54d154
Luiz Fernando <fernandinho.manon@gmail.com>
2024-10-26 20:42:51 -0300

    Curso iniciante JavaScript

---
+++ aula-21/loop_for.js
@@ -0,0 +1,8 @@
+let num; // Criamos uma variável chamada 'num' para armazenar um número
+
+// Inicializamos a variável 'num' com o valor 1. Essa variável será usada como contador.
+for(num = 1; num < 6; num++) {
+    // Esse loop irá se repetir enquanto 'num' será impresso no console.
+    console.log(num);
+    // Após a imporessão, o valor de 'num' é incrementado em 1.
+}
