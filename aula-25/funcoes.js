5b171caba6865082c187627be10c45913d3a154d
Luiz Fernando <fernandinho.manon@gmail.com>
2024-10-30 10:50:02 -0300

    Curso iniciante JavaScript

---
+++ aula-25/funcoes.js
@@ -0,0 +1,9 @@
+// Função para saudar o usuário
+function greetUser() {
+    
+    // Exibe a mensagem de boas-vindas no console 
+    console.log('Bem-vindo ao Curso Web');
+}
+
+// Chama a função para executar a tarefa
+greetUser();
