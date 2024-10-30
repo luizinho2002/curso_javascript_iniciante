c0bda68dad1989e75594794507a706a3f8feca11
Luiz Fernando <fernandinho.manon@gmail.com>
2024-10-30 13:38:55 -0300

    Curso iniciante JavaScript

--- aula-26/funcao2.js
+++ aula-26/funcao2.js
@@ -1,8 +1,12 @@
 function add() {
-    let num1 = 10;
-    let num2 = 5;
-    let resultado = num1 + num2;
-    console.log(`O valor da soma é ${resultado}`);
+    // Declara uma função chamada 'add' que realiza uma operação de adição.
+
+    let num1 = 10; // Declara uma variável 'num1' e atribui o valor 10 a ela.
+    let num2 = 5; //  Declara uma variável 'num2' e atribui o valor 5 a ela.
+
+    let resultado = num1 + num2; // Soma os valores de 'num1' e 'num2' e armazena o resultado na variável 'resultado'.
+
+    console.log(`O valor da soma é ${resultado}`); // Exibe no console o resultado da soma, formatando a mensagem.
 }
 
-add();
+add(); // Chama a função 'add' para executar as operações definidas dentro dela.
