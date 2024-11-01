afbdbc876d6f7c7db8c2140ae2779c6ade702ada
Luiz Fernando <fernandinho.manon@gmail.com>
2024-11-01 15:56:08 -0300

    Curso iniciante JavaScript

---
+++ aula-28/funcao_expressao.js
@@ -0,0 +1,9 @@
+// Cria uma variável chamada 'language' e atribui a ela uma função
+let language = function() {
+
+    // Quando essa função for chamada, ela vai executar essa linha:
+    console.log('JavaScript'); // Imprime a mensagem 'JavaScript' no console
+}
+
+// Chama a função armazenada em 'language'
+language();
