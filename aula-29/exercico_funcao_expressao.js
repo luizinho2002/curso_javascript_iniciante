f7446da276fd5d608b24d6cdc532e96f0f9c001d
Luiz Fernando <fernandinho.manon@gmail.com>
2024-11-01 16:05:35 -0300

    Curso iniciante JavaScript

---
+++ aula-29/exercico_funcao_expressao.js
@@ -0,0 +1,16 @@
+/* 
+   Vamos criar uma função  para adicionar dois números e implementá-la usando uma expressão de função.
+*/
+
+// Definindo a função 'soma'
+const soma = function(num1, num2) {
+
+   // Dentro da função, somando os dois números recebidos como parâmetros.
+   return num1 + num2;
+};
+
+// Chamando a função 'soma' com os valores 5 e 3 e armazenando o resultado.
+const resultado = soma (5, 3);
+
+// Imprimindo o resultado no console.
+console.log(resultado);
